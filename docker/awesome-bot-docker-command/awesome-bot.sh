c9900ded91def14a1fbbaf4e1026c719dff2c029
Jamie York <ziadoz@gmail.com>
2022-11-19 23:49:07 +0000

    til(docker): Awesome Bot Docker Command

---
+++ docker/awesome-bot-docker-command/awesome-bot.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker run -ti --rm -v $PWD:/mnt dkhamsing/awesome_bot --allow-dupe --white-list igor.io,symfony,toranproxy.com,vagrantup.com,3v4l.org,voicesoftheelephpant.com,drupal.org,oreilly.com README.md
